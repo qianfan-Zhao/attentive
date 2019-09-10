44a4fabf76cc578869406120ec52ec723cf1c12b
qianfan Zhao <qianfanguijin@163.com>
2019-09-10 15:24:43 +0800

    example: basic: Take care of usb serial port
    The usb serial port maybe pulled out at anytimes.
    
    Signed-off-by: qianfan Zhao <qianfanguijin@163.com>

--- example/basic.c
+++ example/basic.c
@@ -32,10 +32,44 @@ const char *commands[] = {
     NULL,
 };
 
+/*
+ * Wait device ready and disable modem's echo.
+ */
+static int wait_device_disable_echo(struct at *at, int timeout)
+{
+    const char *resp;
+    int fail;
+
+    while (prog_exit == 0 && timeout-- > 0) {
+        resp = at_command(at, "ATE0");
+        fail = errno;
+
+        if (resp && !strcmp(resp, "")) {
+            printf("device ready\n");
+            at_command(at, "ATE0"); /* make sure ATE0 executed */
+            break;
+        } else if (fail == ENODEV) {
+            return -1;
+        } else {
+            printf(".");
+            fflush(stdout);
+        }
+        sleep(1);
+    }
+
+    return timeout > 0 ? 0 : -1;
+}
+
 int main(int argc, char *argv[])
 {
+    const char *serialport = argv[1];
     const char **cmd, *resp;
     struct at *at;
+
+    if (argc < 2 || !serialport) {
+        fprintf(stderr, "Usage: %s serialport\n", argv[0]);
+        return -1;
+    }
 
     if (!(at = at_alloc_unix())) {
         fprintf(stderr, "alloc at unix failed\n");
@@ -44,39 +78,47 @@ int main(int argc, char *argv[])
 
     signal(SIGINT, signal_handler);
     at_set_timeout(at, 1);
-
-    if (at_open(at, argv[1], B115200) < 0) {
-        fprintf(stderr, "open %s failed: %m\n", argv[1]);
-        prog_exit = 1;
-        at_free(at);
-    }
     at_set_callbacks(at, &callbacks, NULL);
 
-    /* waiting until modem startup and then disable echo */
     while (prog_exit == 0) {
-        resp = at_command(at, "ATE0");
-        if (resp && !strcmp(resp, "")) {
-                printf("device ready\n");
-                at_command(at, "ATE0"); /* make sure ATE0 executed */
+    again:
+        /* Waiting usb serial port enumed */
+        while (prog_exit == 0) {
+            if (!at_open(at, serialport, B115200)) {
                 break;
-        } else {
-                printf(".");
-                fflush(stdout);
-        }
-        sleep(1);
-    }
-
-    while (prog_exit == 0) {
-        for (cmd = commands; *cmd != NULL; cmd++) {
-            resp = at_command(at, "%s", *cmd);
-            if (!resp) {
-                fprintf(stderr, "=> %s no responds: %m\n", *cmd);
             } else {
-                printf("=> %s: %s\n", *cmd, resp);
+                printf("+");
+                fflush(stdout);
             }
+            sleep(5);
         }
 
-        sleep(10);
+        if (wait_device_disable_echo(at, 240) < 0) {
+            fprintf(stderr, "waiting device %s timeout, exit\n", serialport);
+            break;
+        }
+
+        while (prog_exit == 0) {
+            for (cmd = commands; *cmd != NULL; cmd++) {
+                resp = at_command(at, "%s", *cmd);
+                int fail = errno;
+
+                if (!resp) { /* maybe usb serial port pulled out */
+                    printf("=> %s: %m\n", *cmd);
+                    if (fail == ENODEV) {
+                        printf("device %s was pulled out", serialport);
+                        at_close(at);
+                        goto again;
+                    } else if (fail == ETIMEDOUT) {
+                        goto again;
+                    }
+                } else {
+                    printf("=> %s: %s\n", *cmd, resp);
+                }
+            }
+
+            sleep(5);
+        }
     }
 
     at_close(at);
